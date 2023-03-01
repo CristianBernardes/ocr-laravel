7d06413be6b23ebf7650f96448f12eb9a1d767bc
Cristian Anderson Oliveira Bernardes <cristian.aob@gmail.com>
2023-03-01 09:28:31 -0300

    Update OcrController.php

--- app/Http/Controllers/OcrController.php
+++ app/Http/Controllers/OcrController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers;
 
 use thiagoalessio\TesseractOCR\TesseractOCR;
+use thiagoalessio\TesseractOCR\TesseractOcrException;
 
+/**
+ *
+ */
 class OcrController extends Controller
 {
+    /**
+     * @return void
+     * @throws TesseractOcrException
+     */
     public function tesseractOcr()
     {
         $ocr = new TesseractOCR();
